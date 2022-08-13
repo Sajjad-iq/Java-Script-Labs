26d25b88cfb2e3540d9a246e5508daa999a51870
Sajjad-iq <rasooool59@gmail.com>
2022-08-13 16:19:52 +0300

    add

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 /* 
+day 1
 1. Write a JavaScript program to display the current day and time in the following format */
 
 function DayAndTime() {
@@ -39,4 +40,38 @@ function untilChristmas() {
     console.log(sum * 30 - date.getDate())
 }
 
-untilChristmas()
+
+
+
+// day 2
+/* Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50 */
+function def(int1, int2) {
+    if (int1 + int2 == 50 || int1 == 50 || int2 == 50) {
+        return true
+    } else {
+        return false
+    }
+}
+
+/* Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string. */
+function AddPy(string) {
+    let add = string
+    if (add[0] == "P" && add[1] == "y") {
+        return add
+    } else {
+        return `Py${add}`
+    }
+}
+
+/* Write a JavaScript program to remove a character at the specified position of a given string and return the new string */
+function removeChr(str, index) {
+    let string = str.split("")
+    for (var i = 0; i < string.length; i++) {
+        if (i == index) {
+            string.splice(i, 1);
+        }
+    }
+    return string.join("")
+}
+
+console.log(removeChr("hello", 2))
